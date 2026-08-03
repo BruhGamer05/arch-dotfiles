0bcf3b5b5f17191967bb508943fb8d8b9f06f51c
BruhGamer05 <akashkumar050506@gmail.com>
2026-08-03 20:37:47 +0530

    Modularized the files

---
+++ hypr/.config/hypr/conf/autostart.lua
@@ -0,0 +1,8 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+hl.on("hyprland.start", function ()
+    hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
+    hl.exec_cmd("waybar")
+    hl.exec_cmd("mako")
+    hl.exec_cmd("hyprpaper")
+end)
