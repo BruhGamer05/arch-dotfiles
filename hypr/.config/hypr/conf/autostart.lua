08c8097eac879e0ac69001d4302972fe82a8244d
BruhGamer05 <akashkumar050506@gmail.com>
2026-08-14 00:02:48 +0530

    modifications

--- hypr/.config/hypr/conf/autostart.lua
+++ hypr/.config/hypr/conf/autostart.lua
@@ -1,7 +1,7 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd("nm-applet")
     hl.exec_cmd("waybar")
-    hl.exec_cmd("swww-daemon")
+    hl.exec_cmd("awww-daemon")
     hl.exec_cmd("blueman-applet")
     hl.exec_cmd("swaync")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
