3dcd143799970971289a65fc763f3a3d964e623c
BruhGamer05 <akashkumar050506@gmail.com>
2026-08-14 15:02:33 +0530

    Added installation script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Starting Hyprland Rice Installation..."
+
+# 1. Install all packages from the master list
+echo "Installing packages from pkglist.txt..."
+yay -S --needed - < pkglist.txt
+
+# 2. Link all configurations using Stow
+echo "Symlinking dotfiles..."
+stow cava fastfetch gtk gtk2 hypr kitty matugen rofi swaync waybar wlogout zsh
+
+echo "Installation complete! Please reboot your system."
